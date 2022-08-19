859d78f03220a90c7a267dd0a335e87a011387ca
pkamra97 <109149360+pkamra97@users.noreply.github.com>
2022-08-19 22:28:14 +0530

    callback

---
+++ file.js
@@ -0,0 +1,19 @@
+// // console.log("script is working from a diff file")
+// // input 
+// var user=prompt("give me your user name")
+// // process
+// var welcomeMsg="this script works"+user
+// // output
+
+// alert(welcomeMsg)
+
+var btntranslate=document.querySelector("#btn-trans")
+
+// console.log(btntranslate)
+
+btntranslate.addEventListener("click", function clickEventHandler(){
+
+    console.log("clicked")
+
+})
+

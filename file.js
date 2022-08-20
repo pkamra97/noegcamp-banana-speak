40c6a5715494ca9ddc920824f9f61e2fd86915c2
pkamra97 <109149360+pkamra97@users.noreply.github.com>
2022-08-20 10:46:26 +0530

    input output processing done

--- file.js
+++ file.js
@@ -9,16 +9,24 @@
 
 var btntranslate=document.querySelector("#btn-trans")
 var txtInput=document.querySelector("#txt-input")
+var outputDiv=document.querySelector("#output")
 
-console.log(txtInput)
+// outputDiv.innerText="prateek kamra "
+// above line will display the text in output box 
+
+
+// console.log(outputDiv)
+// console.log(txtInput)
 
 // console.log(btntranslate)
 function clickEventHandler(){
 
-    console.log("clicked");
+    outputDiv.innerText="fshbcsjacluw " + txtInput.value;
 
-    // this below line is used for reading input from user via document
-    console.log("input",txtInput.value);
+    // console.log("clicked");
+
+    // // this below line is used for reading input from user via document
+    // console.log("input",txtInput.value);
 
 }
 
